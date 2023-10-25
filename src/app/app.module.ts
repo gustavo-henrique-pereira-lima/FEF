934e2dc65882288695c9578469fef53c80328079
gustavo-henrique-pereira-lima <gustavohenriquelive12@gmail.com>
2023-10-24 23:54:37 -0300

    Eboço do Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
